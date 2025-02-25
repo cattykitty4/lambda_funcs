bafb43c1cb98140f783d6a9aa194ab26fa3185b8
cattykitty4 <aspectmastezzima@gmail.com>
2025-02-25 20:46:43 +1100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 from .anagram import isAnagramm
 from .if_greater_than_string_length import checkIsDigit
+from .if_greater_than_string_length import findNumber
 from .len_stirngs_with_title import totalLenNames
 from .list_with_min_and_max_len import minMaxLen
 from .positives_and_negatives import numsSum
 
-__all__ = ('isAnagramm', 'checkIsDigit', 'totalLenNames', 'minMaxLen', 'numsSum')
+__all__ = ('isAnagramm', 'checkIsDigit','findNumber', 'totalLenNames', 'minMaxLen', 'numsSum')
